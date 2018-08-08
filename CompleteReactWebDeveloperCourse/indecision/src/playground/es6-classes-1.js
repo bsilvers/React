ab2f2e85057ec35de56a738a2085281bc507bae0
Brian Silvers <bsilvers@yahoo.com>
2018-08-07 22:16:35 -0500

    es6 classes

--- CompleteReactWebDeveloperCourse/indecision/src/playground/es6-classes-1.js
+++ CompleteReactWebDeveloperCourse/indecision/src/playground/es6-classes-1.js
@@ -1,11 +1,64 @@
 
 class Person {
-    constructor(name) {
+    
+    constructor(name = 'Anonymous', age = 0) {
         this.name = name;
+        this.age = age;
     }
 
+    getGreeting() {
+        return `Hi. I am ${this.name}!`;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old.`;
+    }
 }
 
-const me = new Person('Brian Silvers');
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if(this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
 
-console.log(me);
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if(this.hasHomeLocation()) {
+            greeting += ` I am visiting from ${this.homeLocation}`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Student('Brian Silvers', 46, 'Computer Science');
+// console.log(me.getGreeting());
+console.log(me.getDescription());
+
+const other = new Student();
+//console.log(other.getGreeting());
+console.log(other.getDescription());
+
+const me2 = new Traveler('Brian Silvers', 46, 'Chicago');
+console.log(me2);
+console.log(me2.getGreeting());
