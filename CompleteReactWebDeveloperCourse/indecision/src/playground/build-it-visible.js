33cde1e9819c456b24fb82fd18c0eae60977763a
Brian Silvers <brian.silvers@aa.com>
2018-08-11 19:58:05 -0500

    togglevisibility

--- CompleteReactWebDeveloperCourse/indecision/src/playground/build-it-visible.js
+++ CompleteReactWebDeveloperCourse/indecision/src/playground/build-it-visible.js
@@ -1,43 +1,71 @@
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+        this.state = {
+            visibility = false
+        };
+    }
 
-const app = {
-    title: 'Visibility Toggle',
-    subTitle: 'Put visibility into your life',
-    hideDetails: true
-};
+    handleToggleVisibility() {
 
-const showDetails = () => {
-    app.hideDetails = false;
-    console.log(app.hideDetails);
-    render();
-};
+    }
 
-const hideDetails = () => {
-    app.hideDetails = true;
-    console.log(app.hideDetails);
-    render();
-};
-
-let visibility = false;
-
-const toggleVisibility = () => {
-    visibility = !visibility;
-    render();
-}
-
-const appRoot = document.getElementById('app');
-
-const render = () => {
-    const template = (
+    render() {
+       return (
         <div>
-            <h1>{app.title}</h1>
-            
-            <button onClick={toggleVisibility}>
-                { visibility ? 'Hide details' : 'Show details' };
-            </button>
-            <p hidden={!visibility}>{app.subTitle}</p>
-        </div>
-    );
-    ReactDOM.render(template, appRoot);
-};
+        <h1>{app.title}</h1>
+        
+        <button onClick={this.toggleVisibility}>
+            { visibility ? 'Hide details' : 'Show details' };
+        </button>
+        <p hidden={!visibility}>{app.subTitle}</p>
+    </div>
+       );
+    }
+}
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
 
-render();
+
+// const app = {
+//     title: 'Visibility Toggle',
+//     subTitle: 'Put visibility into your life',
+//     hideDetails: true
+// };
+
+// const showDetails = () => {
+//     app.hideDetails = false;
+//     console.log(app.hideDetails);
+//     render();
+// };
+
+// const hideDetails = () => {
+//     app.hideDetails = true;
+//     console.log(app.hideDetails);
+//     render();
+// };
+
+// let visibility = false;
+
+// const toggleVisibility = () => {
+//     visibility = !visibility;
+//     render();
+// }
+
+// const appRoot = document.getElementById('app');
+
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>{app.title}</h1>
+            
+//             <button onClick={toggleVisibility}>
+//                 { visibility ? 'Hide details' : 'Show details' };
+//             </button>
+//             <p hidden={!visibility}>{app.subTitle}</p>
+//         </div>
+//     );
+//     ReactDOM.render(template, appRoot);
+// };
+
+// render();
