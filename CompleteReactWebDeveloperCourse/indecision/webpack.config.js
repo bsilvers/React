4cd089f1513104239ac364273cc51443705b7149
Brian Silvers <bsilvers@yahoo.com>
2018-08-16 22:10:27 -0500

    added webpack initial

---
+++ CompleteReactWebDeveloperCourse/indecision/webpack.config.js
@@ -0,0 +1,11 @@
+// entry -> output 
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    }
+};
