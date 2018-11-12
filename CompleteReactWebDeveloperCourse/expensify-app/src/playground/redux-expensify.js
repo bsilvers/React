8ed08788f11e7d5c9660530e74250041d6fbc65c
Brian Silvers <brian.silvers@aa.com>
2018-11-12 13:25:42 -0600

    object spread operator

--- CompleteReactWebDeveloperCourse/expensify-app/src/playground/redux-expensify.js
+++ CompleteReactWebDeveloperCourse/expensify-app/src/playground/redux-expensify.js
@@ -21,16 +21,25 @@ const addExpense = (
         creaetdAt
     }
 });
-
+// REMOVE_EXPENSE
 const removeExpense = ({id} = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 
 });
 
-// REMOVE_EXPENSE
+
 // EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -49,7 +58,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
         ];
         case 'REMOVE_EXPENSE':
             return  state.filter( ({ id }) => id !== action.id);
-            
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if( expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    }
+                } else {
+                    return expense;
+                }
+            });
         default:
             return state;
     }
@@ -65,6 +84,11 @@ const filtersReducerDefaultState  = {
  };
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch(action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default:
         return state;
     }
@@ -88,6 +112,11 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 
 store.dispatch(removeExpense( {id: expenseOne.expense.id}));
 
+store.dispatch(editExpense( expenseTwo.expense.id, { amount: 500}) );
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter(''));
+
 console.log(store.getState());
 
 const demoState = {
@@ -105,3 +134,4 @@ const demoState = {
         endDate: undefined
     }
 };
+
